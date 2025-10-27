3bf010a642933afb251b709fc8aef040ef03267e
Hoid <tylerlcheek@gmail.com>
2025-10-27 13:25:47 -0400

    Implement usage on Mac (currently broken)

--- line-bisect.plugin.zsh
+++ line-bisect.plugin.zsh
@@ -1,15 +1,36 @@
 function line-bisect-left {
-  NEW_CURSOR=`expr $#LBUFFER / 2`
+  NEW_CURSOR=$(($#LBUFFER / 2))
   CURSOR=$NEW_CURSOR
+}
+function line-bisect-right {
+  NEW_CURSOR_OFFSET=$(($#RBUFFER / 2))
+  CURSOR=$((CURSOR + NEW_CURSOR_OFFSET))
+}
+function set-up-windows {
+  bindkey '^[[1;3D' line-bisect-left
+  bindkey '^[[1;3C' line-bisect-right
+}
+function set-up-mac {
+  bindkey '^[b' line-bisect-left
+  bindkey '^[f' line-bisect-right
 }
 
 zle -N line-bisect-left
-bindkey '^[[1;3D' line-bisect-left
-
-function line-bisect-right {
-  NEW_CURSOR_OFFSET=`expr $#RBUFFER / 2`
-  CURSOR=`expr $CURSOR + $NEW_CURSOR_OFFSET`
-}
-
 zle -N line-bisect-right
-bindkey '^[[1;3C' line-bisect-right
+
+unameOut="$(uname -s)"
+echo "${unameOut}"
+case "${unameOut}" in
+Linux*)
+  echo "No Linux setup fn. Please create one in $ZSH/custom/plugins/line-bisect."
+  ;;
+Darwin*)
+  set-up-mac
+  ;;
+CYGWIN*)
+  set-up-windows
+  ;;
+*)
+  echo "machine=UNKNOWN:${unameOut}"
+  ;;
+esac
